ee5030f96f5699be61df0dc74784f4cb5c6b0744
Dmytro <82054589+Dmytro-Yablochkin@users.noreply.github.com>
2021-04-27 21:21:59 +0300

    Update UserData.py

--- page_objects/UserData.py
+++ page_objects/UserData.py
@@ -3,21 +3,21 @@ from GenerateRandomData import random_number
 
 
 class UserData:
-    user_name = 'Дмитрий'
-    user_last_name = 'Яблочкин'
-    user_middle_name = 'Валентинович'
-    user_email = 'Ramennewlife1994@gmail.com'
-    user_password = '1605f1994'
-    user_phone_number = '0632083449'
+    user_name = ''
+    user_last_name = ''
+    user_middle_name = ''
+    user_email = ''
+    user_password = ''
+    user_phone_number = ''
 
     random_user_email = random_email
     random_phone_number = random_number
 
-    user_city = 'Льв'
-    warehouse_num = '№67'
+    user_city = ''
+    warehouse_num = '№'
 
-    current_num = '+38 (063) 208-34-49'
+    current_num = ''
 
     # ----------- incorrect data -------------- #
-    bad_user_email = '12345678910'
-    bad_user_password = '11111111'
+    bad_user_email = ''
+    bad_user_password = ''
